50649397a07e3e888abd89440b7565ec1b807d4d
John Godley <johng75@gmail.com>
2010-12-03 17:33:43 +0000

    First import
    git-svn-id: http://plugins.svn.wordpress.org/rename-media/trunk@318649 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,38 @@
+=== Plugin Name ===
+Contributors: johnny5
+Donate link: http://urbangiraffe.com/about/support/
+Tags: media, image, rename, management
+Requires at least: 2.9
+Tested up to: 3.1
+Stable tag: trunk
+
+Rename underlying media files from the WordPress media management interface
+
+== Description ==
+
+Enhances the WordPress media library by allowing you to rename the underlying media files. Any change to a media item's title will result in the media filename also being changed.
+
+See the screenshot for more details of where to rename media files.
+
+Note that all thumbnails will also be renamed. If you are using a caching plugin, such as WP Super Cache, then you will need to clear your cache so that any references are updated. This plugin does not update existing direct links to files (links inside a WordPress gallery will change).
+
+== Installation ==
+
+The plugin is simple to install:
+
+1. Download `rename-media.zip`
+1. Unzip
+1. Upload html-purified directory to your `/wp-content/plugins` directory
+1. Go to the plugin management page and enable the plugin
+1. Rename a media item by changing the title from the WordPress media management interface
+
+You can find full details of installing a plugin on the [plugin installation page](http://urbangiraffe.com/articles/how-to-install-a-wordpress-plugin/).
+
+== Screenshots ==
+
+1. Changing the media title will result in the filename also changing
+
+== Changelog ==
+
+= 0.1 =
+* Initial release
---
+++ rename-media.php
@@ -0,0 +1,67 @@
+<?php
+/*
+Plugin Name: Rename Media
+Plugin URI: http://urbangiraffe.com/plugins/rename-media/
+Description: Rename underlying media files from the WordPress media management interface
+Version: 0.1
+Author: John Godley
+Author URI: http://urbangiraffe.com
+*/
+
+function rename_media_save( $post, $attachment ) {
+	$old = get_attached_file( $post['ID'] );
+
+	$ext = str_replace( 'jpeg', 'jpg', pathinfo( basename( $old ), PATHINFO_EXTENSION ) );
+	$new = wp_unique_filename( dirname( $old ), $attachment['post_title'].'.'.$ext );
+	$new = dirname( $old ).DIRECTORY_SEPARATOR.strtolower( $new );
+
+	if ( $post['post_name'] != sanitize_title( $attachment['post_title'] ) ) {
+		// Ensure attachment page title changes
+		$post['post_name'] = sanitize_title( $attachment['post_title'] );
+
+		// Save
+		wp_update_post( $post );
+		
+		$new_url = get_permalink( $post['ID'] );
+		
+		$post['guid'] = $new_url;
+		if ( isset( $_REQUEST['_wp_original_http_referer'] ) ) {
+			$_REQUEST['_wp_original_http_referer'] = $post['guid'];
+		}
+
+		$meta = wp_get_attachment_metadata( $post['ID'] );
+
+		// Rename the original file
+		$old_filename = basename( $old );
+		$new_filename = basename( $new );
+
+		$meta['file'] = str_replace( $old_filename, $new_filename, $meta['file'] );
+		
+		// Check if new file exists
+		if ( file_exists( $new ) === false ) {
+			rename( $old, $new );
+		
+			// Rename the sizes
+			$old_filename = pathinfo( basename( $old ), PATHINFO_FILENAME );
+			$new_filename = pathinfo( basename( $new ), PATHINFO_FILENAME );
+		
+			foreach ( $meta['sizes'] AS $size => $meta_size ) {
+				$old_file = dirname( $old ).DIRECTORY_SEPARATOR.$meta['sizes'][$size]['file'];
+			
+				$meta['sizes'][$size]['file'] = str_replace( $old_filename, $new_filename, $meta['sizes'][$size]['file'] );
+
+				$new_file = dirname( $old ).DIRECTORY_SEPARATOR.$meta['sizes'][$size]['file'];
+			
+				rename( $old_file, $new_file );
+			}
+
+			wp_update_attachment_metadata( $post['ID'], $meta );
+
+			update_attached_file( $post['ID'], $new );
+		}
+	}
+
+	return $post;
+}
+
+add_filter( 'attachment_fields_to_save', 'rename_media_save', 10, 2 );
